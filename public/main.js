6ae26f0d2e9ff053e1f2f37f317dd8950996cd20
Erin <eteague27@gmail.com>
2015-12-18 17:59:15 -0500

    added javaScript to home and about

--- public/main.js
+++ public/main.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
-	
+	$("#name").fadeIn(3000);
+	$("#img_home").fadeIn(3500);
+	$("#navbar").slideDown();
 
+	$("#image_about").fadeIn(3000);
+	$("#text_box").fadeIn();
 
 
 });
